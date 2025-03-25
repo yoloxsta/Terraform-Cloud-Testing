cb9004182137ee476cc9e8f76662d622439cc271
Soe Tint Aung <soetintaunghz10@gmail.com>
2025-03-25 09:08:28 +0630

    add:tf

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "AWS_ACCESS_KEY" {
+     type  = string
+     description = "Access key for aws environment"
+}
+
+variable "AWS_SECRET_KEY" {
+     type  = string
+     description = "Secret key for aws environment"
+}
+
+variable "region" {
+  type        = string
+
+}
