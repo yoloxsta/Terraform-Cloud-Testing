73198e5265a0f24c6243ea7c7944972aeda0a936
Soe Tint Aung <soetintaunghz10@gmail.com>
2025-06-24 10:29:17 +0630

    delete tf

--- outputs.tf
+++
@@ -1,7 +0,0 @@
-output "clumsy-bird-url" {
-  value = "http://${aws_eip.clumsy_bird.public_dns}:8001"
-}
-
-output "clumsy-bird-ip" {
-  value = "http://${aws_eip.clumsy_bird.public_ip}:8001"
-}
--- terraform.tfvars
+++
@@ -1,4 +0,0 @@
-prefix        = "app"
-project       = "clumsy-bird"
-environment   = "development"
-instance_type = "t2.micro"
